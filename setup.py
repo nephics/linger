6b016b3a6fc231f481abe7418a23ad351e3cdf5d
Jacob Svensson <jacob@nephics.com>
2018-11-26 11:32:00 +0100

    long description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(name='linger',
       version='1.0.0',
       description='Message queue and pubsub service with HTTP API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Jacob Svensson',
       author_email='jacob@nephics.com',
       license="http://www.apache.org/licenses/LICENSE-2.0",
